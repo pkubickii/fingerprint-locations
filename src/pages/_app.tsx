0757d5a7ddc1ce3d51b9297c4fd72b56b1f8551d
pkubickii <kubicki.przemyslaw@gmail.com>
2022-11-19 21:31:23 +0100

    refactor: moved context providers to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,25 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
+import { useState } from "react";
 import { SessionProvider } from "next-auth/react";
-
+import { EditIdContext } from "../context/EditIdContext";
+import { ModalContext } from "../context/ModalContext";
 import { trpc } from "../utils/trpc";
-
 import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [editId, setEditId] = useState("");
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ModalContext.Provider value={{ showModal, setShowModal }}>
+        <EditIdContext.Provider value={{ editId, setEditId }}>
+          <Component {...pageProps} />
+        </EditIdContext.Provider>
+      </ModalContext.Provider>
     </SessionProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
-import { EditIdContext } from "../context/EditIdContext";
-import { ModalContext } from "../context/ModalContext";
 import EditModal from "../components/EditModal";
 import AuthWrapper from "../components/AuthWrapper";
 import FingerLocations from "../components/FingerLocations";
 import FormUpdateFingerPrint from "../components/FormUpdateFingerPrint";
+import { ModalContext } from "../context/ModalContext";
+import { EditIdContext } from "../context/EditIdContext";
+import { useContext } from "react";
 
 const Home: NextPage = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [editId, setEditId] = useState("");
-
+  const modalContext = useContext(ModalContext);
+  const editIdContext = useContext(EditIdContext);
   return (
     <>
       <Head>
@@ -19,35 +18,25 @@ const Home: NextPage = () => {
         <meta name="description" content="fingerprint locations" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ModalContext.Provider value={{ showModal, setShowModal }}>
-        <EditIdContext.Provider value={{ editId, setEditId }}>
-          <main className="justify-top-5 container mx-auto flex min-h-screen flex-col items-center p-4">
-            <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
-              Fingerprint <span className="text-sky-400">locations</span>
-            </h1>
-            <div>
-              <FingerLocations />
-            </div>
-            <div className="mt-8">
-              <AuthWrapper />
-            </div>
-            <button
-              className="mt-5 rounded-md border-2 border-zinc-800 from-sky-100 to-sky-500 p-2 hover:bg-gradient-to-br focus:outline-none"
-              onClick={() => setShowModal(true)}
-            >
-              Modal
-            </button>
-            <div>
-              <EditModal
-                isVisible={showModal}
-                onClose={() => setShowModal(false)}
-              >
-                <FormUpdateFingerPrint id={editId} />
-              </EditModal>
-            </div>
-          </main>
-        </EditIdContext.Provider>
-      </ModalContext.Provider>
+      <main className="justify-top-5 container mx-auto flex min-h-screen flex-col items-center p-4">
+        <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
+          Fingerprint <span className="text-sky-400">locations</span>
+        </h1>
+        <div>
+          <FingerLocations />
+        </div>
+        <div className="mt-8">
+          <AuthWrapper />
+        </div>
+        <div>
+          <EditModal
+            isVisible={modalContext?.showModal ?? false}
+            onClose={() => modalContext?.setShowModal(false)}
+          >
+            <FormUpdateFingerPrint id={editIdContext?.editId ?? ""} />
+          </EditModal>
+        </div>
+      </main>
     </>
   );
 };
